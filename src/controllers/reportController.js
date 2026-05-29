f131878d0d48a6e3e51304e8067755fe392d3864
Rasyid Prayogo <113403962+rasyid32@users.noreply.github.com>
2026-05-30 01:45:50 +0700

    Revert "5230411123 farhan habibi"

--- src/controllers/reportController.js
+++
@@ -1,5 +0,0 @@
-/**
- * File ini dikerjakan oleh Anggota 3 untuk CRUD laporan kerusakan.
- */
-
-module.exports = {};
--- src/controllers/userController.js
+++
@@ -1,6 +0,0 @@
-/**
- * File ini dikerjakan oleh Anggota 2 untuk fitur profile user dan
- * pengelolaan data user sesuai role.
- */
-
-module.exports = {};
